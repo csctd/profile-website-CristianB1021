1e1da81402a229cab9890327be157839852cced1
CristianB1021 <141265968+CristianB1021@users.noreply.github.com>
2023-08-04 08:58:11 -0400

    add author info

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Mr. Bonilla'
+copyright = '2027, Cristian Bonilla'
+author = 'Cristian Bonilla'
 
 
 
